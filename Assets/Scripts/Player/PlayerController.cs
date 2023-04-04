e9148f2fb99215a67596be974e1c090d6d5d27b7
dimasabreu <dimasabreu@live.com>
2023-04-04 20:36:20 -0300

    logica da animacao

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,12 +9,28 @@ public class PlayerController : MonoBehaviour
     private Vector3 direcao;
     [SerializeField]
     private int velocidade = 10;
+    private  Animator animator;
+    private void Start() 
+    {
+        animator = GetComponent<Animator>();
+    }
     void Update()
     {
         
         eixoX = Input.GetAxis("Horizontal");
         eixoZ = Input.GetAxis("Vertical");
+
         direcao = new Vector3(eixoX, 0, eixoZ);
+
         transform.Translate(direcao * velocidade * Time.deltaTime);
+
+        if(direcao != Vector3.zero)
+        {
+            animator.SetBool("Moving", true);
+        }
+        else
+        {
+            animator.SetBool("Moving", false);
+        }
     }
 }
